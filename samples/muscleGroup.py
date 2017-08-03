d8f0bb35d4f18d287568fd0e27920cb0a53bb033
Sonny <>
2017-08-03 12:13:56 +0900

    Adding sample objects

---
+++ samples/muscleGroup.py
@@ -0,0 +1,11 @@
+from enum import Enum
+class MuscleGroup(Enum):
+    CHEST = 1
+    BICEPS = 2
+    ABS = 3
+    QUADRICEPS = 4
+    HAMSTRINGS = 5
+    SHOULDERS = 6
+    TRICEPS = 7
+    UPPERBACK = 8
+    LOWERBACK = 9
